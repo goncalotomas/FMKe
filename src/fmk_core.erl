1f4aec32af68f537e95621e79a4a59953a7e847f
goncalotomas <ga.tomas@outlook.pt>
2016-04-24 17:16:49 +0100

    upgrade tests, can now be run multiple times

--- src/fmk_core.erl
+++ src/fmk_core.erl
@@ -4,7 +4,7 @@
 -export([write_to_antidote/3,read_from_antidote/2]). %%TODO finish
 
 write_to_antidote(Key,Type,Params) ->
-	rpc:call(?ANTIDOTE,antidote,append,[Key,Type,{Params,4}]).
+	rpc:call(?ANTIDOTE,antidote,append,[Key,Type,{Params,self()}]).
 
 read_from_antidote(Key,Type) ->
 	rpc:call(?ANTIDOTE,antidote,read,[Key,Type]).
--- test/testidote.erl
+++ test/testidote.erl
@@ -8,9 +8,11 @@
 
 %% ============================================================================
 %% EUnit tests
+%% TODO: initializer test should be a fixture.
+%% See https://github.com/erlang/rebar3/issues/1166
 %% ============================================================================
 -ifdef(TEST).
-testidote_test() ->
+initializer_test() ->
   net_kernel:start(['testidote@127.0.0.1',longnames]),
   erlang:set_cookie(node(),antidote).
 
@@ -18,10 +20,14 @@ antidote_ping_test() ->
   ?assertEqual(pong, net_adm:ping(?ANTIDOTE)).
 
 counter_test() ->
-  fmk_core:write_to_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE, {increment, 1}),
-  fmk_core:write_to_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE, {increment, 1}),
-  fmk_core:write_to_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE, {increment, 1}),
-  ?assertEqual({ok,3}, fmk_core:read_from_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE)).
+  {ok,_} = fmk_core:write_to_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE, increment),
+  {ok,_} = fmk_core:write_to_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE, {increment, 2}),
+  {ok,_} = fmk_core:write_to_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE, {increment, 3}),
+  ?assertEqual({ok,6}, fmk_core:read_from_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE)),
+  fmk_core:write_to_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE, decrement),
+  fmk_core:write_to_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE, {decrement, 2}),
+  fmk_core:write_to_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE, {decrement, 3}),
+  ?assertEqual({ok,0}, fmk_core:read_from_antidote(?TEST_COUNTER_KEY,?COUNTER_TYPE)).
 
 map_test() ->
   RegisterUpdate = {update,{key, riak_dt_lwwreg},{assign, <<"Awesome">>}},
