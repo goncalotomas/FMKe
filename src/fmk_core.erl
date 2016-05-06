97c1f8bd36d521e4af48801020a429de6bad2ec2
goncalotomas <ga.tomas@outlook.pt>
2016-05-06 13:00:01 +0100

    fix find_patient, finish create_patient

--- src/fmk_core.erl
+++ src/fmk_core.erl
@@ -3,7 +3,7 @@
 
 -export([
   find_patient/1,
-  create_patient_bucket/1,
+  create_patient/3,
   update_patient/1,
   read_patient/1
   ]).
@@ -11,11 +11,17 @@
 %% Finds a patient in the Antidote Key-Value store by Patient ID.
 -spec find_patient(Id::pos_integer()) -> riak_dt_map:map().
 find_patient(Id) ->
-  Patients = antidote_lib:read_from_antidote(?FMK_PATIENTS,riak_dt_map),
+  Patients = antidote_lib:get(?FMK_PATIENTS,riak_dt_map),
   case lists:keyfind({Id,riak_dt_map},1,Patients) of
     false -> not_found;
     {{Id,riak_dt_map},Patient} -> Patient
   end.
+
+create_patient(Id,Name,Address) ->
+  Patient = patient:new(Id,Name,Address),
+  NestedMapOp = antidote_lib:build_map_op(Id,riak_dt_map,Patient),
+  MapUpdate = antidote_lib:build_map_update([NestedMapOp]),
+  ok = antidote_lib:put(?FMK_PATIENTS,riak_dt_map,MapUpdate).
 
 create_patient_bucket(PatientId) ->
   antidote_lib:create_object(PatientId,riak_dt_map).
