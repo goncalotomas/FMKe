0a36c1332342685455f9d8f35066eca84332db1d
Gonçalo Tomás <goncalo@goncalotomas.com>
2016-10-09 17:47:22 +0100

    add spec definition for events

--- src/event.erl
+++ src/event.erl
@@ -18,6 +18,7 @@
 %% as well as a date when the event occured and also a small description.
 %% All Ids must be of type pos_integer() and the Timestamp and Description should
 %% be supplied in binary
+-spec new(id(),id(),id(),binary(),binary()) -> [map_field_update()].
 new(Id,PatientId,StaffMemberId,Timestamp,Description) ->
   IdOp = build_id_op(?EVENT_ID,?EVENT_ID_CRDT,Id),
   PatientNameOp = build_id_op(?EVENT_PATIENT_ID,?EVENT_PATIENT_ID_CRDT,PatientId),
@@ -27,22 +28,27 @@ new(Id,PatientId,StaffMemberId,Timestamp,Description) ->
   [IdOp,PatientNameOp,StaffMemberNameOp,TimestampOp,DescriptionOp].
 
 %% Returns the patient ID from an already existant event object.
+-spec patient_id(crdt()) -> id().
 patient_id(Event) ->
   antidote_lib:find_key(Event,?EVENT_PATIENT_ID,?EVENT_PATIENT_ID_CRDT).
 
 %% Returns the event ID from an already existant event object.
+-spec id(crdt()) -> id().
 id(Event) ->
   antidote_lib:find_key(Event,?EVENT_ID,?EVENT_ID_CRDT).
 
 %% Returns the event timestamp from an already existant event object.
+-spec timestamp(crdt()) -> binary().
 timestamp(Event) ->
   antidote_lib:find_key(Event,?EVENT_TIMESTAMP,?EVENT_TIMESTAMP_CRDT).
 
 %% Returns the staff ID from an already existant event object.
+-spec staff_id(crdt()) -> id().
 staff_id(Event) ->
   antidote_lib:find_key(Event,?EVENT_STAFF_ID,?EVENT_STAFF_ID_CRDT).
 
 %% Returns the description from an already existant event object.
+-spec description(crdt()) -> binary().
 description(Event) ->
   antidote_lib:find_key(Event,?EVENT_ID,?EVENT_ID_CRDT).
 
