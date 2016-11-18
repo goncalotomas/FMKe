b1a580614ef3fb1f7166757eef19f491846933b7
goncalotomas <goncalo@goncalotomas.com>
2016-11-18 13:10:40 +0000

    add GET, PUT and POST endpoint for facilities

--- src/facility_handler.erl
+++ src/facility_handler.erl
@@ -1,1 +1,101 @@
 -module (facility_handler).
+-include ("fmk_http.hrl").
+
+-export([init/2]).
+
+init(Req0, Opts) ->
+	Method = cowboy_req:method(Req0),
+	HasBody = cowboy_req:has_body(Req0),
+	Req = handle_req(Method, HasBody, Req0),
+	{ok, Req, Opts}.
+
+handle_req(<<"POST">>, true, Req) ->
+		create_facility(Req);
+handle_req(<<"POST">>, false, Req) ->
+		cowboy_req:reply(400, [], ?ERR_MISSING_BODY, Req);
+
+handle_req(<<"PUT">>, true, Req) ->
+		update_facility(Req);
+handle_req(<<"PUT">>, false, Req) ->
+		cowboy_req:reply(400, [], ?ERR_MISSING_BODY, Req);
+
+handle_req(<<"GET">>, true, Req) ->
+		cowboy_req:reply(400, [], ?ERR_BODY_IN_A_GET_REQUEST, Req);
+handle_req(<<"GET">>, false, Req) ->
+		get_facility(Req).
+
+create_facility(Req) ->
+		{ok, [{<<"id">>, FacilityId},
+		{<<"name">>, FacilityName},
+		{<<"address">>, FacilityAddress},
+		{<<"type">>, FacilityType}
+		], _Req0} = cowboy_req:read_urlencoded_body(Req),
+		IntegerId = binary_to_integer(FacilityId),
+		case IntegerId =< ?MIN_ID of
+				true ->
+						cowboy_req:reply(400, [], ?ERR_INVALID_FACILITY_ID, Req);
+				false ->
+						StringName = binary_to_list(FacilityName),
+						StringAddress = binary_to_list(FacilityAddress),
+						StringType = binary_to_list(FacilityType),
+						ServerResponse = fmk_core:create_facility(IntegerId,StringName,StringAddress,StringType),
+						Success = ServerResponse =:= ok,
+						JsonReply =	lists:flatten(io_lib:format(
+								"{\"success\": \"~p\", \"result\": \"~p\"}",
+								[Success,ServerResponse]
+						)),
+						cowboy_req:reply(200, #{
+								<<"content-type">> => <<"application/json">>
+						}, JsonReply, Req)
+		end.
+
+update_facility(Req) ->
+		{ok, [{<<"name">>, Name},
+		{<<"address">>, Address},
+    {<<"type">>, Type}
+		], _Req0} = cowboy_req:read_urlencoded_body(Req),
+		Id = cowboy_req:binding(?BINDING_FACILITY_ID, Req, -1),
+		IntegerId = binary_to_integer(Id),
+		case IntegerId =< ?MIN_ID of
+				true ->
+						cowboy_req:reply(400, [], ?ERR_INVALID_PATIENT_ID, Req);
+				false ->
+						StringName = binary_to_list(Name),
+						StringAddress = binary_to_list(Address),
+            StringType = binary_to_list(Type),
+						ServerResponse = fmk_core:update_facility_details(IntegerId,StringName,StringAddress,StringType),
+						Success = ServerResponse =:= ok,
+						JsonReply =	lists:flatten(io_lib:format(
+								"{\"success\": \"~p\", \"result\": \"~p\"}",
+								[Success,ServerResponse]
+						)),
+						cowboy_req:reply(200, #{
+								<<"content-type">> => <<"application/json">>
+						}, JsonReply, Req)
+		end.
+
+get_facility(Req) ->
+		Id = cowboy_req:binding(?BINDING_FACILITY_ID, Req, -1),
+		IntegerId = binary_to_integer(Id),
+		case IntegerId =< ?MIN_ID of
+				true ->
+						cowboy_req:reply(400, [], ?ERR_INVALID_EVENT_ID, Req);
+				false ->
+						ServerResponse = fmk_core:get_facility_by_id(IntegerId),
+						Success = ServerResponse =/= {error,not_found},
+						JsonReply = case Success of
+								true ->
+										lists:flatten(io_lib:format(
+												("{\"success\": \"~p\", \"result\": " ++ crdt_json_encoder:encode(facility,ServerResponse) ++ "}"),
+												[Success]
+										));
+								false ->
+										lists:flatten(io_lib:format(
+												"{\"success\": \"~p\", \"result\": \"~p\"}",
+												[Success,ServerResponse]
+										))
+						end,
+						cowboy_req:reply(200, #{
+								<<"content-type">> => <<"text/plain">>
+						}, JsonReply, Req)
+		end.
