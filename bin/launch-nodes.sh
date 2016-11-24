7d91e31de1cbefc8cfd61eb494ce6585d0958425
Alejandro Z. Tomsic <aletomsic@gmail.com>
2016-11-24 12:23:43 +0100

    adding scripts to start multi fmk

---
+++ bin/launch-nodes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export RELX_REPLACE_OS_VARS=true
+for i in `seq 1 $1`;
+do
+  PLATFORM_DATA_DIR="data/${i}" RING_STATE_DIR="data/ring/${i}" HANDOFF_PORT=8${i}99 PB_PORT=8${i}87 PUBSUB_PORT=8${i}86 LOGREADER_PORT=8${i}85 INSTANCE_NAME=antidote${i} PB_IP=127.0.0.1 COOKIE=antidote _build/default/rel/fmk${i}/bin/env foreground &
+done
