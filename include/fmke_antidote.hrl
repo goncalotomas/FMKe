dc9c1851ced7b4fcb71e14937590250682454912
Gonçalo Tomás <goncalo@goncalotomas.com>
2017-03-24 11:46:16 +0000

    add antidote specific macros

--- include/fmke_antidote.hrl
+++ include/fmke_antidote.hrl
@@ -1,9 +1,16 @@
-%%%-------------------------------------------------------------------
-%%% @author goncalotomas
-%%% @copyright (C) 2017, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 24. Mar 2017 11:04
-%%%-------------------------------------------------------------------
--author("goncalotomas").
+-define(DEFAULT_ANTIDOTE_PORT, "[8087]").
+-define(DEFAULT_ANTIDOTE_ADDRESS, "[127.0.0.1]").
+-define(VAR_ANTIDOTE_PB_PID, antidote_pb_pid).
+-define(VAR_ANTIDOTE_PB_ADDRESS, antidote_pb_address).
+-define(VAR_ANTIDOTE_PB_PORT, antidote_pb_port).
+
+%% Useful shortcut macros
+-define (BCOUNTER, antidote_crdt_bcounter).
+-define (counter, antidote_crdt_counter).
+-define (GSET, antidote_crdt_gset).
+-define (LWWREG, antidote_crdt_lwwreg).
+-define (MAP, antidote_crdt_gmap).
+-define (NESTED_MAP, antidote_crdt_gmap).
+-define (MVREG, antidote_crdt_mvreg).
+-define (ORSET, antidote_crdt_orset).
+-define (RGA, antidote_crdt_rga).
