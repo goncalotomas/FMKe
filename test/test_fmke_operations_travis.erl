0d39c8a788aed736359e9fce08c9e5d0d493889d
Gonçalo Tomás <goncalo@goncalotomas.com>
2017-05-08 16:20:25 +0100

    add very basic prescription tests (many more to come)

--- test/test_fmke_operations_travis.erl
+++ test/test_fmke_operations_travis.erl
@@ -1,5 +1,6 @@
 -module (test_fmke_operations_travis).
 -include("fmk.hrl").
+-include("fmk_kv.hrl").
 
 -ifdef(TEST).
 -compile(export_all).
@@ -19,6 +20,7 @@
 -define (STATIC_STAFF_NAME_UPDATED, "FMKe doctor name updated").
 -define (STATIC_STAFF_SPECIALITY, "FMKe doctor speciality").
 -define (STATIC_STAFF_SPECIALITY_UPDATED, "FMKe doctor speciality updated").
+-define (UNDEFINED_FIELD, <<"undefined">>).
 -endif.
 
 -ifdef(TEST).
@@ -26,9 +28,9 @@
 connection_test_() ->
     {"Runs a quick ping test between the current test node and the local FMKe node",
     {setup,
-      fun start/0,               % setup function
-      fun stop/1,                % teardown function
-      fun test_connection/1      % instantiator
+      fun start/0,                              % setup function
+      fun stop/1,                               % teardown function
+      fun test_connection/1                     % instantiator
     }}.
 
 patient_operations_test_() ->
@@ -37,7 +39,7 @@ patient_operations_test_() ->
     {setup,
       fun start/0,                              % setup function
       fun stop/1,                               % teardown function
-      fun run_patient_operations/1         % instantiator
+      fun run_patient_operations/1              % instantiator
     }}.
 
 pharmacy_operations_test_() ->
@@ -64,7 +66,16 @@ staff_operations_test_() ->
     {setup,
       fun start/0,                              % setup function
       fun stop/1,                               % teardown function
-      fun run_staff_operations/1             % instantiator
+      fun run_staff_operations/1                % instantiator
+    }}.
+
+prescription_operations_test_() ->
+    {"Runs a sequential list of operations that expose most of the prescription record"
+    " functionality and that serve as unit tests for those operations.",
+    {setup,
+      fun start/0,                              % setup function
+      fun stop/1,                               % teardown function
+      fun run_prescription_operations/1         % instantiator
     }}.
 
 start() ->
@@ -198,6 +209,73 @@ run_staff_operations(FmkeNode) ->
         get_static_staff(FmkeNode))
     ].
 
+run_prescription_operations(FmkeNode) ->
+    %% generating required fields to pattern match on later
+    BinStaticId = integer_to_binary(?STATIC_ID),
+    DatePrescribed1 = gen_rand_date(),
+    DatePrescribed2 = gen_rand_date(),
+    Drugs1 = gen_drugs(),
+    Drugs2 = gen_drugs(),
+    [
+    %% assumes that data store already contains basic entities such as patients,
+    %% hospitals, doctors and pharmacies.
+    %% get unexistant first prescription
+    ?_assertEqual({error,not_found},get_static_prescription(FmkeNode,?STATIC_ID))
+    %% normal create for first prescription
+    ,?_assertEqual(ok,create_static_prescription(FmkeNode,?STATIC_ID,DatePrescribed1,Drugs1))
+    %% create when record already exists
+    ,?_assertEqual({error,prescription_id_taken},create_static_prescription(FmkeNode,?STATIC_ID,DatePrescribed2,Drugs2))
+    %% normal get for first prescription
+    ,?_assert(verify_prescription_fields(FmkeNode,?STATIC_ID,DatePrescribed1,?UNDEFINED_FIELD,Drugs1,?PRESCRIPTION_NOT_PROCESSED_VALUE))
+
+    % %% update record fields
+    % ,?_assertEqual(ok,update_static_prescription(FmkeNode))
+    % %% get record after update
+    % ,?_assertEqual(
+    %     #prescription{
+    %       id=integer_to_binary(?STATIC_ID),
+    %       name=list_to_binary(?STATIC_STAFF_NAME_UPDATED),
+    %       address=list_to_binary(?STATIC_ADDRESS_UPDATED),
+    %       speciality=list_to_binary(?STATIC_STAFF_SPECIALITY_UPDATED)
+    %     },
+    %     get_static_prescription(FmkeNode))
+    ].
+
+
+verify_prescription_fields(FmkeNode,Id,DatePrescribed,DateProcessed,Drugs,IsProcessed) when is_list(DatePrescribed) ->
+    verify_prescription_fields(FmkeNode,Id,list_to_binary(DatePrescribed),DateProcessed,Drugs,IsProcessed);
+
+verify_prescription_fields(FmkeNode,Id,DatePrescribed,DateProcessed,Drugs,IsProcessed) when is_list(DateProcessed) ->
+    verify_prescription_fields(FmkeNode,Id,DatePrescribed,list_to_binary(DateProcessed),Drugs,IsProcessed);
+
+verify_prescription_fields(FmkeNode,Id,DatePrescribed,DateProcessed,Drugs,IsProcessed) ->
+    DBPrescription = get_static_prescription(FmkeNode,Id),
+    %% static id is used for the basic entities key
+    BinStaticId = integer_to_binary(?STATIC_ID),
+    cmp_presc_fields(DBPrescription, #prescription{
+      id=BinStaticId
+      ,patient_id = BinStaticId
+      ,pharmacy_id = BinStaticId
+      ,prescriber_id = BinStaticId
+      ,date_prescribed = DatePrescribed
+      ,date_processed = DateProcessed
+      ,drugs = [list_to_binary(X) || X <- Drugs]
+      ,is_processed = IsProcessed
+    }).
+
+cmp_presc_fields(DBPrescription = #prescription{}, Expected = #prescription{}) ->
+    (DBPrescription#prescription.id =:= Expected#prescription.id)
+    and (DBPrescription#prescription.patient_id =:= Expected#prescription.patient_id)
+    and (DBPrescription#prescription.pharmacy_id =:= Expected#prescription.pharmacy_id)
+    and (DBPrescription#prescription.prescriber_id =:= Expected#prescription.prescriber_id)
+    and (DBPrescription#prescription.date_prescribed =:= Expected#prescription.date_prescribed)
+    and (DBPrescription#prescription.date_processed =:= Expected#prescription.date_processed)
+    and (DBPrescription#prescription.is_processed =:= Expected#prescription.is_processed)
+    and cmp_drug_list(DBPrescription#prescription.drugs,Expected#prescription.drugs).
+
+cmp_drug_list(List1, List2) ->
+    lists:sort(List1) =:= lists:sort(List2).
+
 create_static_patient(FmkeNode) ->
     run_generic_create_op(FmkeNode,patient,[?STATIC_ID,?STATIC_PATIENT_NAME,?STATIC_ADDRESS]).
 
@@ -210,6 +288,9 @@ create_static_facility(FmkeNode) ->
 create_static_staff(FmkeNode) ->
     run_generic_create_op(FmkeNode,staff,[?STATIC_ID,?STATIC_STAFF_NAME,?STATIC_ADDRESS,?STATIC_STAFF_SPECIALITY]).
 
+create_static_prescription(FmkeNode,Id,Date,Drugs) ->
+    run_generic_create_op(FmkeNode,prescription,[Id,?STATIC_ID,?STATIC_ID,?STATIC_ID,Date,Drugs]).
+
 get_static_patient(FmkeNode) ->
     run_generic_get_op(FmkeNode,patient).
 
@@ -221,6 +302,9 @@ get_static_facility(FmkeNode) ->
 
 get_static_staff(FmkeNode) ->
     run_generic_get_op(FmkeNode,staff).
+
+get_static_prescription(FmkeNode,Id) ->
+    run_get_op(FmkeNode,prescription,Id).
 
 update_static_patient(FmkeNode) ->
     run_generic_update_op(FmkeNode,patient,[?STATIC_ID,?STATIC_PATIENT_NAME_UPDATED,?STATIC_ADDRESS_UPDATED]).
@@ -243,8 +327,29 @@ run_generic_update_op(FmkeNode,Entity,Args) ->
     run_rpc_op(FmkeNode,OpAtom,Args).
 
 run_generic_get_op(FmkeNode,Entity) ->
+    run_get_op(FmkeNode,Entity,?STATIC_ID).
+
+run_get_op(FmkeNode,Entity,Id) ->
     OpAtom = build_get_op(Entity),
-    run_rpc_op(FmkeNode,OpAtom,[?STATIC_ID]).
+    run_rpc_op(FmkeNode,OpAtom,[Id]).
+
+gen_rand_date() ->
+    Day = rand:uniform(20)+10,
+    Month = rand:uniform(20)+10,
+    Year = rand:uniform(2000)+1000,
+    format_list("~p/~p/~p",[Day,Month,Year]).
+
+gen_drugs() ->
+    NumDrugs = rand:uniform(3)+1,
+    gen_drugs_rec(NumDrugs).
+
+gen_drugs_rec(1) ->
+    [gen_rand_str()];
+gen_drugs_rec(Num) when Num > 1 ->
+    [gen_rand_str()] ++ gen_drugs_rec(Num-1).
+
+gen_rand_str() ->
+    binary_to_list(base64:encode(crypto:strong_rand_bytes(32))).
 
 build_update_op(Entity) ->
     build_generic_op("update_~p_details",[Entity]).
@@ -256,7 +361,10 @@ build_get_op(Entity) ->
     build_generic_op("get_~p_by_id",[Entity]).
 
 build_generic_op(List,Args) ->
-    list_to_atom(lists:flatten(io_lib:format(List,Args))).
+    list_to_atom(format_list(List,Args)).
+
+format_list(List,Args) ->
+    lists:flatten(io_lib:format(List,Args)).
 
 run_rpc_op(FmkeNode, Op, Params) ->
     rpc:block_call(FmkeNode, fmk_core, Op, Params).
