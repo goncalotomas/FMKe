48656dff8af98674011aeeea54514cd3f9594f56
Gonçalo Tomás <goncalo@goncalotomas.com>
2017-05-10 17:27:23 +0100

    separate logic for calling the fmke ops script

---
+++ scripts/run_fmke_operations.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+echo "running FMKe unit tests with $1 back end..."
+./scripts/start_data_store.sh $1
+./scripts/start_fmke.sh
+set +e
+./rebar3 eunit
+if [ $? -ne  0 ]; then
+    set -e
+    echo "fatal: one or more tests failed."
+    ./scripts/stop_fmke.sh
+    ./scripts/stop_data_store.sh $1
+    exit 5
+fi
+./scripts/stop_fmke.sh
+./scripts/stop_data_store.sh $1
+echo "done"
