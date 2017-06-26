83b01aa362302ecaf9237af7a9ddf89a842abbe5
Gonçalo Tomás <goncalo@goncalotomas.com>
2017-06-26 10:53:18 +0100

    remove antidote specific macros and types

--- include/fmk.hrl
+++ include/fmk.hrl
@@ -1,32 +1,13 @@
 %%-define(ANTIDOTE, 'antidote@127.0.0.1').
 -define(DB_DRIVER, fmke_kv_driver).
--define(KV_IMPLEMENTATION, fmke_db_driver_antidote).
+-define(KV_IMPLEMENTATION, fmke_db_driver_riak_kv).
 -define (DEFAULT_FMKE_HTTP_PORT, 9090).
 -define (APP, fmke).
 
-%% Type specification borrowed from antidote
--type txid() :: {pid(), antidote:txid()}.
--type reason() :: antidote:reason().
--type snapshot_time() :: antidote:snapshot_time().
--type bound_object() :: antidote:bound_object().
--type op_name() :: antidote:op_name().
--type op_param() :: antidote:op_param().
--type crdt() :: term().
--type crdt_op() :: any().
--type field() :: binary().
--type map_field_op() ::  {remove, field()}.
--type map_field_update() :: {update, field(), crdt_op()}.
--type map_op() :: {update, {[map_field_update() | map_field_op()], actorordot()}}.
--type actorordot() :: antidote_crdt:actor() | antidote_crdt:dot().
--type object_bucket() :: {field(), crdt(), term()}.
 -type id() :: non_neg_integer().
--define (MAP_UPDATE_OP,update).
-
-%% Test macros
--define(TEST_COUNTER_TYPE, antidote_crdt_pncounter).
--define(TEST_COUNTER_KEY, fmk_counter_test).
--define(TEST_MAP_KEY, 'fmk_map_test').
--define(TEST_NESTED_MAP_KEY, 'fmk_nested_map_test').
+-type field() :: binary().
+-type reason() :: term().
+-type crdt() :: term().
 
 -record(prescription, {
     id :: id()
