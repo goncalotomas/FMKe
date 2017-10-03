37ff5f0701e597a5f732fb44bafb68fe500bccbf
Gonçalo Tomás <goncalo@goncalotomas.com>
2017-10-03 02:59:56 +0100

    implement redis driver, local eunit ok

--- src/fmke_db_driver_redis.erl
+++ src/fmke_db_driver_redis.erl
@@ -68,21 +68,68 @@ commit_transaction({Pid}) ->
     poolboy:checkin(fmke_db_connection_pool, Pid),
     {ok, {}}.
 
-% %% Get
-% %% Redis does not support map that holds keys of different types,
-% %% So just return a dummy object and use find_key to get specific key-value pairs.
-% get(Key, _KeyType, Context) ->
-%     {ok, {map, Key}, Context}.
+%% -------------------------------------------------------------------
+%% Data access exports - Interaction with Redis
+%% -------------------------------------------------------------------
+get_presc_key(patient) -> ?PATIENT_PRESCRIPTIONS_KEY;
+get_presc_key(pharmacy) -> ?PHARMACY_PRESCRIPTIONS_KEY;
+get_presc_key(staff) -> ?STAFF_PRESCRIPTIONS_KEY.
+
+get(Key, prescription, Context) ->
+    Ops = [["HGETALL", Key], ["SMEMBERS", concat_bin_strings([Key, ?PRESCRIPTION_DRUGS_KEY])]],
+    [{ok,Fields}, {ok,Drugs}] = execute_multi_get(Context, Ops),
+    case Fields of
+        [] -> {{error, not_found}, Context};
+        _ ->  {{ok, build_app_record(Fields ++ [?PRESCRIPTION_DRUGS_KEY, Drugs])}, Context}
+    end;
+
+get(Key, KeyType, Context) when KeyType == patient; KeyType == pharmacy; KeyType == staff ->
+    Ops = [["HGETALL", Key], ["KEYS", concat_bin_strings([Key, get_presc_key(KeyType), <<"*">>])]],
+    [{ok,Fields}, {ok,PrescKeys}] = execute_multi_get(Context, Ops),
+    case Fields of
+        [] -> {{error, not_found}, Context};
+        _ ->  case PrescKeys of
+                  [] ->       {{ok, build_app_record(Fields)}, Context};
+                  [_H|_T] ->  Prescs = fetch_nested_prescriptions(Context, lists:sort(PrescKeys)),
+                              io:format("PRESCRIPTIONS: ~p~n", [Prescs]),
+                              {{ok, build_app_record(Fields ++ [get_presc_key(KeyType), Prescs])}, Context}
+              end
+    end;
+
 get(Key, _KeyType, Context) ->
   case execute_get(Context, ["HGETALL", Key]) of
       {ok,[]} -> {{error, not_found}, Context};
       {ok,Res} -> {{ok, build_app_record(Res)}, Context}
   end.
 
+fetch_nested_prescriptions(Context, Prescs) -> fetch_nested_prescriptions(Context, Prescs, []).
+
+fetch_nested_prescriptions(Context, [], Accum) ->
+    Prescs = execute_multi_get(Context, Accum),
+    parse_prescription_list(Prescs);
+fetch_nested_prescriptions(Context, [Key, DrugsKey | T], Accum) ->
+    fetch_nested_prescriptions(Context, T, [["HGETALL", Key], ["SMEMBERS", DrugsKey] | Accum]).
+
+parse_prescription_list(List) ->
+    parse_prescription_list(List, []).
+
+parse_prescription_list([], Accum) ->
+    lists:reverse(Accum);
+parse_prescription_list([{error, _Reason} | T], Accum) ->
+    parse_prescription_list(T, Accum);
+parse_prescription_list([{ok, Fields}, {ok, Drugs} | T], Accum) ->
+    parse_prescription_list(T, [build_app_record(Fields ++ [?PRESCRIPTION_DRUGS_KEY, Drugs]) | Accum]).
+
 build_app_record([?PATIENT_ID_KEY, Id,
                   ?PATIENT_NAME_KEY, Name,
                   ?PATIENT_ADDRESS_KEY, Address]) ->
                       #patient{id=Id, name=Name, address=Address};
+
+build_app_record([?PATIENT_ID_KEY, Id,
+                  ?PATIENT_NAME_KEY, Name,
+                  ?PATIENT_ADDRESS_KEY, Address,
+                  ?PATIENT_PRESCRIPTIONS_KEY, Prescs]) ->
+                      #patient{id=Id, name=Name, address=Address, prescriptions=Prescs};
 
 build_app_record([?FACILITY_ID_KEY, Id,
                   ?FACILITY_NAME_KEY, Name,
@@ -95,24 +142,38 @@ build_app_record([?PHARMACY_ID_KEY, Id,
                   ?PHARMACY_ADDRESS_KEY, Address]) ->
                       #pharmacy{id=Id, name=Name, address=Address};
 
+build_app_record([?PHARMACY_ID_KEY, Id,
+                  ?PHARMACY_NAME_KEY, Name,
+                  ?PHARMACY_ADDRESS_KEY, Address,
+                  ?PHARMACY_PRESCRIPTIONS_KEY, Prescriptions]) ->
+                      #pharmacy{id=Id, name=Name, address=Address, prescriptions=Prescriptions};
+
 build_app_record([?STAFF_ID_KEY, Id,
                   ?STAFF_NAME_KEY, Name,
                   ?STAFF_ADDRESS_KEY, Address,
                   ?STAFF_SPECIALITY_KEY, Speciality]) ->
                       #staff{id=Id, name=Name, address=Address, speciality=Speciality};
 
+build_app_record([?STAFF_ID_KEY, Id,
+                  ?STAFF_NAME_KEY, Name,
+                  ?STAFF_ADDRESS_KEY, Address,
+                  ?STAFF_SPECIALITY_KEY, Speciality,
+                  ?STAFF_PRESCRIPTIONS_KEY, Prescriptions]) ->
+                      #staff{id=Id, name=Name, address=Address, speciality=Speciality, prescriptions=Prescriptions};
+
 build_app_record([?PRESCRIPTION_ID_KEY, Id,
                   ?PRESCRIPTION_PATIENT_ID_KEY, PatientId,
                   ?PRESCRIPTION_PRESCRIBER_ID_KEY, PrescriberId,
                   ?PRESCRIPTION_PHARMACY_ID_KEY, PharmacyId,
-                  ?PRESCRIPTION_DATE_PRESCRIBED_KEY, DatePrescribed]) ->
+                  ?PRESCRIPTION_DATE_PRESCRIBED_KEY, DatePrescribed,
+                  ?PRESCRIPTION_DRUGS_KEY, Drugs]) ->
                       #prescription{
                           id=Id
                           ,patient_id=PatientId
                           ,pharmacy_id=PharmacyId
                           ,prescriber_id=PrescriberId
                           ,date_prescribed=DatePrescribed
-                          ,drugs=[]
+                          ,drugs=Drugs
                       };
 
 build_app_record([?PRESCRIPTION_ID_KEY, Id,
@@ -120,8 +181,9 @@ build_app_record([?PRESCRIPTION_ID_KEY, Id,
                   ?PRESCRIPTION_PRESCRIBER_ID_KEY, PrescriberId,
                   ?PRESCRIPTION_PHARMACY_ID_KEY, PharmacyId,
                   ?PRESCRIPTION_DATE_PRESCRIBED_KEY, DatePrescribed,
+                  ?PRESCRIPTION_IS_PROCESSED_KEY, IsProcessed,
                   ?PRESCRIPTION_DATE_PROCESSED_KEY, DateProcessed,
-                  ?PRESCRIPTION_IS_PROCESSED_KEY, IsProcessed]) ->
+                  ?PRESCRIPTION_DRUGS_KEY, Drugs]) ->
                       #prescription{
                           id=Id
                           ,patient_id=PatientId
@@ -129,26 +191,9 @@ build_app_record([?PRESCRIPTION_ID_KEY, Id,
                           ,prescriber_id=PrescriberId
                           ,date_prescribed=DatePrescribed
                           ,date_processed=DateProcessed
-                          ,drugs=[]
+                          ,drugs=Drugs
                           ,is_processed=IsProcessed
                       }.
-
-find_key({map, Map}, Key, register, Context) ->
-    Query = ["HGET", Map, Key],
-    case execute_get(Context, Query) of
-      {ok, Res} -> {ok, Res, Context};
-      {error, Reason} -> {error, Reason, Context}
-    end;
-find_key({map, Map}, Key, map, Context) ->
-    InnerKey = inner_map_key(Map, Key),
-    {ok, {map, InnerKey}, Context};
-find_key({map, Map}, Key, set, Context) ->
-    InnerKey = inner_map_key(Map, Key),
-    Query = ["SMEMBERS", InnerKey],
-    case execute_get(Context, Query) of
-        {ok, Res} -> {ok, Res, Context};
-        {error, Reason} -> {error, Reason, Context}
-    end.
 
 %% Updates
 put(Key, _KeyType, ListOfOps, Context) ->
@@ -160,11 +205,11 @@ put(Key, _KeyType, ListOfOps, Context) ->
 
 update_nested_objects(Key, ListOfOps, Context) ->
     lists:foreach(fun(Op) ->
-                          case update_nested_object(Key, Op, Context) of
-                              ok -> ok;
-                              {error, Reason} -> throw(erlang:error(Reason))
-                          end
-                       end, ListOfOps).
+                      case update_nested_object(Key, Op, Context) of
+                          ok -> ok;
+                          {error, Reason} -> throw(erlang:error(Reason))
+                      end
+                   end, ListOfOps).
 
 update_nested_object(Map, {create_register, Key, Value}, Context) ->
     Op = ["HSET", Map, Key, Value],
@@ -180,20 +225,26 @@ update_nested_object(Map, {create_map, Key, NestedOps}, Context) ->
     update_nested_objects(InnerKey, NestedOps, Context);
 
 update_nested_object(Map, {update_map, Key, NestedOps}, Context) ->
-   InnerKey = inner_map_key(Map, Key),
-   update_nested_objects(InnerKey, NestedOps, Context).
+    InnerKey = inner_map_key(Map, Key),
+    update_nested_objects(InnerKey, NestedOps, Context).
 
 inner_map_key(Map, Key) ->
     list_to_binary(binary_to_list(Map) ++ binary_to_list(Key)).
 
 execute_op({Pid}, Op) ->
     case eredis:q(Pid,Op) of
-      {ok, _} -> ok;
-      {error, Reason} -> {error, Reason}
+        {ok, _} -> ok;
+        {error, Reason} -> {error, Reason}
     end.
 
 execute_get({Pid}, Op) ->
-   case eredis:q(Pid,Op) of
-       {ok, Res} -> {ok, Res};
-       {error, Reason} -> {error, Reason}
-   end.
+   eredis:q(Pid,Op).
+
+execute_multi_get({Pid}, Ops) ->
+  case eredis:qp(Pid,Ops) of
+      {error, Reason} -> {error, Reason};
+      Res -> Res
+  end.
+
+concat_bin_strings(Keys) ->
+  iolist_to_binary(Keys).
