93461651cfd46eb51b4386c7a93d41fbe1c8367b
Gonçalo Tomás <goncalo@goncalotomas.com>
2018-02-07 18:08:08 +0000

    start app, cowboy, poolboy pools and conn manager

--- src/fmke_sup.erl
+++ src/fmke_sup.erl
@@ -30,49 +30,21 @@ start_link(Args) ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init(_Args) ->
-    read_config_file(),
+    config_env(),
 
-    {ok, HttpPort} = application:get_env(?APP, http_port),
+    ConnPoolSpecs = gen_pool_specs(),
+    ConnManagerSpec = gen_conn_pool_mgr_spec(),
+    WebServerSpec = gen_web_server_spec(),
+    ApplicationSpec = gen_fmke_spec(),
 
-    Dispatch = cowboy_router:compile([
-      {'_', [
-        {"/", fmke_http_handler_app, []},
-        {"/prescriptions/[:id]", fmke_http_handler_prescriptions, []},
-        {"/patients/[:id]", fmke_http_handler_patients, []},
-        {"/pharmacies/[:id]", fmke_http_handler_pharmacies, []},
-        {"/pharmacies/[:id]/prescriptions", fmke_http_handler_pharmacies, prescriptions},
-        {"/pharmacies/[:id]/processed_prescriptions", fmke_http_handler_pharmacies, processed_prescriptions},
-        {"/facilities/[:id]", fmke_http_handler_facilities, []},
-        % {"/treatments/[:id]", fmke_http_handler_treatments, []},
-        % {"/events/[:id]", fmke_http_handler_events, []},
-        {"/staff/[:id]", fmke_http_handler_staff, []},
-        {"/staff/[:id]/prescriptions", fmke_http_handler_staff, prescriptions}
-      ]}
-    ]),
-
-    RestartStrategy = #{strategy => one_for_one, intensity => 5, period => 10},
-
-    WebServerChildSpec = #{
-        id => cowboy,
-        start => {cowboy, start_clear, [fmke_http_listener, [{port, HttpPort}], #{env => #{dispatch => Dispatch}}]},
-        restart => permanent,
-        type => worker
-    },
-
-    ApplicationChildSpec = #{
-        id => fmke,
-        start => {fmke, start_link, [[]]},
-        restart => permanent,
-        type => worker
-    },
-
-    {ok, {RestartStrategy, [WebServerChildSpec, ApplicationChildSpec]}}.
+    RestartStrategy = #{strategy => one_for_one, intensity => 10, period => 10},
+    {ok, {RestartStrategy, [WebServerSpec, ApplicationSpec, ConnManagerSpec | ConnPoolSpecs]}}.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
-read_config_file() ->
+config_env() ->
     try
         {ok, CurrentDirectory} = file:get_cwd(),
         ConfigFile = CurrentDirectory ++ ?CONFIG_FILE_PATH,
@@ -97,7 +69,96 @@ config(ConfigProps) ->
         end,
     ?OPTIONS).
 
+gen_fmke_spec() ->
+    #{
+        id => fmke,
+        start => {fmke, start_link, [[]]},
+        restart => permanent,
+        type => worker
+    }.
+
+gen_web_server_spec() ->
+    {ok, HttpPort} = application:get_env(?APP, http_port),
+
+    Dispatch = cowboy_router:compile([
+      {'_', [
+        {"/", fmke_http_handler_app, []},
+        {"/prescriptions/[:id]", fmke_http_handler_prescriptions, []},
+        {"/patients/[:id]", fmke_http_handler_patients, []},
+        {"/pharmacies/[:id]", fmke_http_handler_pharmacies, []},
+        {"/pharmacies/[:id]/prescriptions", fmke_http_handler_pharmacies, prescriptions},
+        {"/pharmacies/[:id]/processed_prescriptions", fmke_http_handler_pharmacies, processed_prescriptions},
+        {"/facilities/[:id]", fmke_http_handler_facilities, []},
+        % {"/treatments/[:id]", fmke_http_handler_treatments, []},
+        % {"/events/[:id]", fmke_http_handler_events, []},
+        {"/staff/[:id]", fmke_http_handler_staff, []},
+        {"/staff/[:id]/prescriptions", fmke_http_handler_staff, prescriptions}
+      ]}
+    ]),
+
+    #{
+        id => cowboy,
+        start => {cowboy, start_clear, [fmke_http_listener, [{port, HttpPort}], #{env => #{dispatch => Dispatch}}]},
+        restart => permanent,
+        type => worker
+    }.
+
+gen_pool_specs() ->
+    {ok, TargetDatabase} = application:get_env(?APP, target_database),
+    {ok, ConnPoolSize} = application:get_env(?APP, connection_pool_size),
+    {ok, Hostnames} = application:get_env(?APP, database_addresses),
+    {ok, PortNums} = application:get_env(?APP, database_ports),
+
+    ClientLib = get_client_lib(TargetDatabase),
+    {Hosts, Ports} = make_same_len(Hostnames, PortNums),
+    Connections = lists:zip(Hosts, Ports),
+    NumPools = length(Connections),
+    %% Save pool names to application env
+    ok = application:set_env(?APP, pools, lists:map(
+                                                fun(N) ->
+                                                    list_to_atom("pool_" ++ get_atom_compatible_addr(
+                                                                                lists:nth(N, Hosts)))
+                                                end, lists:seq(1, NumPools))),
+    %% Create a connection pool to each database node
+    Pools = lists:map(
+                fun(N) ->
+                    Name = list_to_atom("pool_" ++ get_atom_compatible_addr(
+                                                lists:nth(N, Hosts))),
+                    lager:info("Generating spec for conn pool with name ~p~n", [Name]),
+                    SizeArgs = [{size, ConnPoolSize}, {max_overflow, 2 * ConnPoolSize}],
+                    {Host, Port} = lists:nth(N, Connections),
+                    WorkerArgs = [{client_lib, ClientLib}, {host, Host}, {port, Port}],
+                    {Name, SizeArgs, WorkerArgs}
+                end, lists:seq(1, NumPools)),
+    %% return specs for each pools
+    lists:map(
+        fun({Name, SizeArgs, WorkerArgs}) ->
+            PoolArgs = [{name, {local, Name}}, {worker_module, fmke_db_connection}] ++ SizeArgs,
+            poolboy:child_spec(Name, PoolArgs, WorkerArgs)
+        end, Pools).
+
+gen_conn_pool_mgr_spec() ->
+    #{
+        id => fmke_db_conn_manager,
+        start => {fmke_db_conn_manager, start_link, []},
+        restart => permanent,
+        type => worker
+    }.
+
 get_value(false, undefined, undefined, Val) -> {defaults, Val};
 get_value(false, undefined, Val, _) -> {config_file, Val};
 get_value(false, {ok, Val}, _, _) -> {app_env, Val};
 get_value(Val, _, _, _) -> {os_env, Val}.
+
+make_same_len(L1, L2) when length(L1) == length(L2) -> {L1, L2};
+make_same_len([H1|_T1] = L1, L2) when length(L1) < length(L2) -> make_same_len([H1 | L1], L2);
+make_same_len(L1, [H2|_T2] = L2) when length(L1) > length(L2) -> make_same_len(L1, [H2 | L2]).
+
+-spec get_client_lib(Database :: antidote | riak | redis) -> atom().
+get_client_lib(antidote) -> antidotec_pb_socket;
+get_client_lib(riak) -> riakc_pb_socket;
+get_client_lib(redis) -> eredis.
+
+-spec get_atom_compatible_addr(Addr :: list()) -> list().
+get_atom_compatible_addr(Addr) ->
+    lists:flatten(string:replace(lists:flatten(string:replace(Addr, ".", ":", all)), ":", "_", all)).
