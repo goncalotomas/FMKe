43bd474e3af57fae5281892eda245bc63a51ca69
Gonçalo Tomás <goncalo@goncalotomas.com>
2018-03-13 15:25:09 +0000

    implement simple ets driver

--- src/fmke_driver_ets.erl
+++ src/fmke_driver_ets.erl
@@ -18,79 +18,87 @@
   stop/0,
   start_transaction/1,
   commit_transaction/2,
-  get/3,
-  put/4
+  get/2,
+  put/2
 ]).
 
 -define(SERVER, ?MODULE).
--define(TABLE_NAME, fmke_ets).
 
 start(DataModel) ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [], [DataModel]).
+    gen_server:start_link({local, ?SERVER}, ?MODULE, DataModel, []).
 
 stop() ->
-  gen_server:call(?MODULE, stop).
+    gen_server:call(?MODULE, stop).
 
-init([DataModel]) ->
-  ets:new(?TABLE_NAME, []),
-  {ok, DataModel}.
+init(DataModel) ->
+    lager:info("~p starting with ~p data model~n", [?MODULE, DataModel]),
+    {ok, DataModel}.
 
 start_transaction(_Opts) ->
-  {ok, []}.
+    {ok, []}.
 
 commit_transaction(_Context, _Opts) ->
-  {ok, []}.
+    {ok, []}.
 
-get(Key, Type, _Context) ->
-  gen_server:call(?MODULE, {get, Key, Type}).
+get(Keys, _Context) ->
+    gen_server:call(?MODULE, {get, Keys}).
 
-put(Key, Type, Value, _Context) ->
-  ets:insert(?TABLE_NAME, {Key, unpack(nested, Type, Value)}).
+put(Entries, _Context) ->
+    gen_server:call(?MODULE, {put, Entries}).
 
-handle_call({get, Key, Type}, _From, DataModel) ->
-  {Key, Result} = ets:lookup(?TABLE_NAME, Key),
-  {reply, pack(DataModel, Result, Type), DataModel};
+handle_call({get, Keys}, _From, DataModel) ->
+    Results = lists:map(
+                    fun({Key, Type}) ->
+                        case ets:lookup(?ETS_TABLE_NAME, Key) of
+                            [] -> {error, not_found};
+                            [{Key, Value}] -> pack(DataModel, Value, Type)
+                        end
+                    end, Keys),
+    {reply, {Results, []}, DataModel};
 
-handle_call({put, Key, Type, Value}, _From, DataModel) ->
-  ets:insert(?TABLE_NAME, {Key, unpack(nested, Type, Value)}),
-  {reply, ok, DataModel}.
+handle_call({put, Entries}, _From, DataModel) ->
+    Results = lists:map(
+                    fun({Key, Type, Value}) ->
+                        true = ets:insert(?ETS_TABLE_NAME, {Key, unpack(nested, Type, Value)}),
+                        ok
+                    end, Entries),
+    {reply, {Results, []}, DataModel}.
 
 handle_cast(_Msg, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 pack(_, {Id, Name, Address, Type}, facility) ->
-  #facility{id = Id, name = Name, address = Address, type = Type};
+    #facility{id = Id, name = Name, address = Address, type = Type};
 pack(_, {Id, Name, Address, Prescriptions}, patient) ->
-  #patient{id = Id, name = Name, address = Address, prescriptions = Prescriptions};
+    #patient{id = Id, name = Name, address = Address, prescriptions = Prescriptions};
 pack(_, {Id, Name, Address, Prescriptions}, pharmacy) ->
-  #pharmacy{id = Id, name = Name, address = Address, prescriptions = Prescriptions};
+    #pharmacy{id = Id, name = Name, address = Address, prescriptions = Prescriptions};
 pack(_, {Id, PatientId, PrescriberId, PharmacyId,
                  DatePrescribed, DateProcessed, Drugs, IsProcessed}, prescription) ->
-  #prescription{
-      id = Id
-      ,patient_id = PatientId
-      ,prescriber_id = PrescriberId
-      ,pharmacy_id = PharmacyId
-      ,date_prescribed = DatePrescribed
-      ,date_processed = DateProcessed
-      ,drugs = Drugs
-      ,is_processed = IsProcessed
-  };
+    #prescription{
+        id = Id
+        ,patient_id = PatientId
+        ,prescriber_id = PrescriberId
+        ,pharmacy_id = PharmacyId
+        ,date_prescribed = DatePrescribed
+        ,date_processed = DateProcessed
+        ,drugs = Drugs
+        ,is_processed = IsProcessed
+    };
 pack(_, {Id, Name, Address, Speciality, Prescriptions}, staff) ->
-  #staff{id = Id, name = Name, address = Address, speciality = Speciality, prescriptions = Prescriptions}.
-
+    #staff{id = Id, name = Name, address = Address, speciality = Speciality, prescriptions = Prescriptions}.
 
 unpack(_, facility, #facility{id = Id, name = Name, address = Address, type = Type}) ->
-  {Id, Name, Address, Type};
+    {Id, Name, Address, Type};
 unpack(_, patient, #patient{id = Id, name = Name, address = Address, prescriptions = Prescriptions}) ->
-  {Id, Name, Address, Prescriptions};
+    {Id, Name, Address, Prescriptions};
 unpack(_, pharmacy, #pharmacy{id = Id, name = Name, address = Address, prescriptions = Prescriptions}) ->
-  {Id, Name, Address, Prescriptions};
+    {Id, Name, Address, Prescriptions};
 unpack(_, prescription, #prescription{id = Id, patient_id = PatientId, prescriber_id = PrescriberId,
                                            pharmacy_id = PharmacyId, date_prescribed = DatePrescribed,
                                            date_processed = DateProcessed, drugs = Drugs,
                                            is_processed = IsProcessed}) ->
-  {Id, PatientId, PrescriberId, PharmacyId, DatePrescribed, DateProcessed, Drugs, IsProcessed};
+    {Id, PatientId, PrescriberId, PharmacyId, DatePrescribed, DateProcessed, Drugs, IsProcessed};
 unpack(nested, staff, #staff{id = Id, name = Name, address = Address, speciality = Speciality,
                              prescriptions = Prescriptions}) ->
-  {Id, Name, Address, Speciality, Prescriptions}.
+    {Id, Name, Address, Speciality, Prescriptions}.
