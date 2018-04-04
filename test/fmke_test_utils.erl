80817cc9879f6484e50df9a0adc9379088616136
Gonçalo Tomás <goncalo@goncalotomas.com>
2018-04-04 13:26:46 +0100

    improve fmke_test_utils API

--- test/fmke_test_utils.erl
+++ test/fmke_test_utils.erl
@@ -7,12 +7,11 @@
     start_antidote/0,
     start_riak/0,
     start_redis/0,
-    start_node_with_antidote_backend/1,
-    start_node_with_riak_backend/1,
-    start_node_with_redis_backend/1,
-    start_norm_node_with_antidote_backend/1,
-    start_norm_node_with_riak_backend/1,
-    start_node_with_mock_redis_cluster/1,
+    start_node_with_antidote_backend/3,
+    start_node_with_ets_backend/2,
+    start_node_with_redis_backend/3,
+    start_node_with_riak_backend/3,
+    start_node_with_mock_redis_cluster/3,
     stop_all/0,
     stop_antidote/0,
     stop_riak/0,
@@ -20,13 +19,14 @@
     stop_node/1
 ]).
 
--define (ANTIDOTE_PORT, 8087).
--define (RIAK_PORT, 8087).
--define (REDIS_PORT, 6379).
+-define(ANTIDOTE_PORT, 8087).
+-define(RIAK_PORT, 8087).
+-define(REDIS_PORT, 6379).
 
 start_antidote() ->
     Result = os:cmd("docker run -d --name antidote -e NODE_NAME=antidote@127.0.0.1 "
-           "-p \"4368:4368\" -p \"8085:8085\" -p \"8087:8087\" -p \"8099:8099\" -p \"9100:9100\" mweber/antidotedb"),
+                    "-p \"4368:4368\" -p \"8085:8085\" -p \"8087:8087\" -p \"8099:8099\" -p \"9100:9100\" "
+                    "mweber/antidotedb"),
     io:format("Starting antidote...~n~p~n", [Result]),
     timer:sleep(5000).
 
@@ -34,10 +34,10 @@ stop_antidote() ->
     os:cmd("docker stop antidote && docker rm antidote").
 
 start_riak() ->
-    Result = os:cmd("docker run -d --name riak -p \"8087:8087\" -p \"8098:8098\" -e NODE_NAME=riak@127.0.0.1"
-            " goncalotomas/riak"),
+    Result = os:cmd("docker run -d --name riak -p \"8087:8087\" -p \"8098:8098\" "
+                    "-e NODE_NAME=riak@127.0.0.1 goncalotomas/riak"),
     io:format("Starting riak...~n~p~n", [Result]),
-    timer:sleep(17500).
+    timer:sleep(30000).
 
 stop_riak() ->
     os:cmd("docker stop riak && docker rm riak").
@@ -54,54 +54,46 @@ stop_all() ->
     Result = os:cmd("docker stop $(docker ps -aq) && docker rm $(docker ps -aq)"),
     io:format("Stopping all containers...~n~p~n", [Result]).
 
-start_norm_node_with_antidote_backend(Name) ->
+start_node_with_antidote_backend(Name, Optimized, DataModel) ->
     fmke_test_utils:start_antidote(),
-    start_local_node(Name, antidote_norm, ?ANTIDOTE_PORT).
+    start_node(Name, [{optimized_driver, Optimized}, {data_model, DataModel}, {target_database, antidote},
+                      {database_ports, [?ANTIDOTE_PORT]}]).
 
-start_norm_node_with_riak_backend(Name) ->
+start_node_with_ets_backend(Name, DataModel) ->
+    start_node(Name, [{data_model, DataModel}, {target_database, ets}]).
+
+start_node_with_riak_backend(Name, Optimized, DataModel) ->
     fmke_test_utils:start_riak(),
-    start_local_node(Name, riak_norm, ?RIAK_PORT).
+    start_node(Name, [{optimized_driver, Optimized}, {data_model, DataModel}, {target_database, riak},
+                      {database_ports, [?RIAK_PORT]}]).
 
-start_node_with_antidote_backend(Name) ->
-    fmke_test_utils:start_antidote(),
-    start_local_node(Name, antidote, ?ANTIDOTE_PORT).
-
-start_node_with_riak_backend(Name) ->
-    fmke_test_utils:start_riak(),
-    start_local_node(Name, riak, ?RIAK_PORT).
-
-start_node_with_redis_backend(Name) ->
+start_node_with_redis_backend(Name, Optimized, DataModel) ->
     fmke_test_utils:start_redis(),
-    start_local_node(Name, redis, ?REDIS_PORT).
+    start_node(Name, [{optimized_driver, Optimized}, {data_model, DataModel}, {target_database, redis},
+                      {database_ports, [?REDIS_PORT]}]).
 
-start_node_with_mock_redis_cluster(Name) ->
+start_node_with_mock_redis_cluster(Name, Optimized, DataModel) ->
     fmke_test_utils:start_redis(),
     %% Uses two different loopback addresses to create pools (one IPv4, one IPv6)
-    start_local_node(Name, redis, ["127.0.0.1", "::1"], [?REDIS_PORT, ?REDIS_PORT]).
+    start_node(Name, [{optimized_driver, Optimized}, {data_model, DataModel}, {target_database, redis},
+                      {database_addresses, ["127.0.0.1", "::1"]}, {database_ports, [?REDIS_PORT]}]).
 
-start_local_node(Name, Database, Port) ->
-    start_local_node(Name, Database, ["127.0.0.1"], [Port]).
-
-start_local_node(Name, Database, Hostnames, Ports) ->
-    io:format(
-        "Trying to spawn node ~p and connect it to ~p...~n"
-        "Using list of hostnames: ~p...~n"
-        "Using list of ports: ~p~n",
-        [Name, Database, Hostnames, Ports]),
+start_node(Name, Opts) ->
     CodePath = lists:filter(fun filelib:is_dir/1, code:get_path()),
     %% have the slave nodes monitor the runner node, so they can't outlive it
     NodeConfig = [{monitor_master, true}, {kill_if_fail, true}, {boot_timeout, 5}, {init_timeout, 3},
         {startup_timeout, 1}, {startup_functions, [{code, set_path, [CodePath]}]}],
+    %% start ct_slave node
     case ct_slave:start(Name, NodeConfig) of
         {ok, Node} ->
-            ok = rpc:call(Node, application, set_env, [?APP, target_database, Database]),
-            ok = rpc:call(Node, application, set_env, [?APP, connection_pool_size, 64]),
-            ok = rpc:call(Node, application, set_env, [?APP, database_addresses, Hostnames]),
-            ok = rpc:call(Node, application, set_env, [?APP, database_ports, Ports]),
-            ok = rpc:call(Node, application, set_env, [?APP, http_port, 9090]),
+            StartupOpts = lists:ukeymerge(1, lists:sort(Opts), lists:sort(maps:to_list(?DEFAULTS))),
+            lists:map(
+                fun({Opt, Val}) ->
+                    rpc:call(Node, application, set_env, [?APP, Opt, Val])
+                end, StartupOpts),
 
-            ClientLib = get_client_lib(Database),
-            ok = rpc:call(Node, application, load, [ClientLib]),
+            {target_database, Database} = lists:keyfind(target_database, 1, StartupOpts),
+            ok = load_client_lib(Node, Database),
             %% start the application remotely
             {ok, _} = rpc:call(Node, application, ensure_all_started, [?APP]),
             io:format("Node ~p started", [Node]),
@@ -110,7 +102,7 @@ start_local_node(Name, Database, Hostnames, Ports) ->
             io:format("Error starting node ~p (~p), retrying...", [Node, Reason]),
             ct_slave:stop(Name),
             wait_until_offline(Node),
-            start_local_node(Name, Database, Hostnames, Ports)
+            start_node(Name, Opts)
     end.
 
 wait_until_offline(Node) ->
@@ -136,9 +128,10 @@ stop_node(Node) ->
     ct_slave:stop(Node),
     wait_until_offline(Node).
 
--spec get_client_lib(Database :: antidote | antidote_norm | riak | riak_norm | redis) -> atom().
-get_client_lib(antidote) -> antidote_pb;
-get_client_lib(antidote_norm) -> antidote_pb;
-get_client_lib(riak) -> riak_pb;
-get_client_lib(riak_norm) -> riak_pb;
-get_client_lib(redis) -> eredis.
+
+load_client_lib(Node, antidote) ->          rpc:call(Node, application, load, [antidote_pb]);
+load_client_lib(Node, antidote_norm) ->     load_client_lib(Node, antidote);
+load_client_lib(_Node, ets) ->              ok;
+load_client_lib(Node, riak) ->              rpc:call(Node, application, load, [riak_pb]);
+load_client_lib(Node, riak_norm) ->         load_client_lib(Node, riak);
+load_client_lib(Node, redis) ->             rpc:call(Node, application, load, [eredis]).
